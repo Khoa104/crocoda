e2596be743480af0cdc5df6629cec00ad6975554
Khoa104 <nguyendangkhoa104@gmail.com>
2025-10-09 00:11:09 +0700

    fix vercel deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "./",
+  build: {
+    outDir: 'dist'
+  },
   server: {
     allowedHosts: ['crocoda.qzz.io', 'crocoda-dev.vercel.app', 'http://localhost:5173/']
   }
